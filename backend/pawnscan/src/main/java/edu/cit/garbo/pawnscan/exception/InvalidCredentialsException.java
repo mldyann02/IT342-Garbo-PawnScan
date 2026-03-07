bfbfa05c3e8f8a30b46f1b26a29c39f3dd203b35
LAB13_USER ON WS17 <L13Y09W17@LabHCI.net>
2026-03-07 08:54:53 +0800

    backend: initial-backend-setup-for-authentication

---
+++ backend/pawnscan/src/main/java/edu/cit/garbo/pawnscan/exception/InvalidCredentialsException.java
@@ -0,0 +1,8 @@
+package edu.cit.garbo.pawnscan.exception;
+
+public class InvalidCredentialsException extends RuntimeException {
+
+    public InvalidCredentialsException(String message) {
+        super(message);
+    }
+}
